9c599e46a6fc6086d94a9c780fc4af8eded92eca
Mohd Suhail <Suhail.work71@gmail.com>
2025-08-16 23:14:21 +0530

    updated 15august with responsive

--- components/HomeTabBar.tsx
+++ components/HomeTabBar.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { productType } from "@/constant";
 import { Repeat } from "lucide-react";
+
 interface Props {
   selectedTab: string;
   onTabSelect: (tab: string) => void;
@@ -8,21 +9,30 @@ interface Props {
 
 const HomeTabbar = ({ selectedTab, onTabSelect }: Props) => {
   return (
-    <div className="flex items-center gap-1.5 text-sm font-semibold">
-      <div className="flex items-center gap-1.5">
+    <div className="text-sm font-semibold">
+      {/* Grid for mobile, flex for desktop */}
+      <div
+        className="
+          grid grid-cols-3 gap-2
+          md:flex md:flex-wrap md:gap-1.5
+        "
+      >
         {productType?.map((item) => (
           <button
             onClick={() => onTabSelect(item?.title)}
             key={item?.title}
-            className={`border border-[#151515] px-4 py-1.5 md:px-6 md:py-2 rounded-full hover:bg-[#151515] hover:text-white cursor-pointer hoverEffect ${selectedTab === item?.title && "bg-[#151515] text-white"}`}
+            className={`border border-[#151515] px-4 py-1.5 md:px-6 md:py-2 rounded-full hover:bg-[#151515] hover:text-white cursor-pointer hoverEffect ${
+              selectedTab === item?.title && "bg-[#151515] text-white"
+            }`}
           >
             {item?.title}
           </button>
         ))}
+
+        <button className="border border-[#151515] px-2 py-2 rounded-full hover:bg-[#151515] hover:text-white hoverEffect flex justify-center items-center">
+          <Repeat className="w-5 h-5" />
+        </button>
       </div>
-      <button className="border border-[#151515] px-2 py-2 rounded-full hover:bg-[#151515] hover:text-white hoverEffect">
-        <Repeat className="w-5 h-5" />
-      </button>
     </div>
   );
 };
--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -1,10 +1,33 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import HomeTabBar from "./HomeTabBar";
 import { productType } from "@/constant";
+import { client } from "@/sanity/lib/client";
+
 
 export const ProductGrid = () => {
+
+  const [loading, setLoading] = useState(false);
   const [selectedTab, setSelectTab] = useState(productType[0]?.title || "");
+  const query = `*[_type == "product" && variant == $variant] | order(name asc)`;
+  const params = { variant: selectedTab.toLowerCase() };
+
+   useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await client.fetch(query, params);
+        // setProducts(await response);
+        console.log(response);
+        
+      } catch (error) {
+        console.log("Product fetching Error", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [selectedTab]);
   return (
     <div className="mt-10 flex flex-col items-center">
       <HomeTabBar
