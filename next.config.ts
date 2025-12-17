2038acf2dbefccecc5b5a70eee521817256160af
cromyofficial <cromyofficial@gmail.com>
2025-12-17 15:28:19 +0530

    12decafter

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,23 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [{ protocol: "https", hostname: "cdn.sanity.io" }],
+//   },
+//   eslint: {
+//     ignoreDuringBuilds: true, // ← lets the build pass even if lint errors exist
+//   },
+// };
+
+// export default nextConfig;
+
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ protocol: "https", hostname: "cdn.sanity.io" }],
-  },
-  eslint: {
-    ignoreDuringBuilds: true, // ← lets the build pass even if lint errors exist
   },
 };
 
