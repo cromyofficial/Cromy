2dbfe83a2b48b6e164a0ec8da864d79388871a67
cromyofficial <cromyofficial@gmail.com>
2026-05-10 16:16:23 +0530

    remove: unnecessary Google verification HTML file

--- app/(Client)/layout.tsx
+++ app/(Client)/layout.tsx
@@ -1,49 +1,3 @@
-// // "use client"
-// import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
-// import "../globals.css";
-// import Header from "@/components/Header";
-// import { Footer } from "@/components/Footer";
-// import { ClerkProvider } from "@clerk/nextjs";
-// import localFont from "next/font/local";
-// import { Toaster } from "react-hot-toast";
-
-// const raleway = localFont({
-//   src: "../fonts/Raleway.woff2",
-//   variable: "--font-raleway",
-//   weight: "100 900",
-// });
-
-// export const metadata: Metadata = {
-//   title: "Cromy",
-//   description: "An E-commerce web for buy jeans purpose ",
-// };
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <ClerkProvider>
-//       <html lang="en">
-//         <body className={`${raleway.variable}antialiased`}>
-//           <Header />
-//           {children}
-//           <Footer />
-//            <Toaster position="bottom-right" toastOptions={{
-//             style:{
-//               background: "#000000",
-//               color: "#ffffff",
-//             }
-//            }}/>
-//         </body>
-//       </html>
-//     </ClerkProvider>
-//   );
-// }
-
-
 import type { Metadata, Viewport } from "next";
 import "../globals.css";
 import Header from "@/components/Header";
@@ -116,6 +70,9 @@ export const metadata: Metadata = {
       "max-snippet": -1,
       "max-video-preview": -1,
     },
+  },
+    verification: {
+    google: "Jr8DwFVA7v6krZDxSK-fxZ6GDzjwC4byA9vjnd2q51s",
   },
   icons: {
     icon: "/favicon.ico",
