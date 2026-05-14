63f89f0fc6c6386c977362dbf43fef3f9ac8159d
cromyofficial <cromyofficial@gmail.com>
2026-05-14 21:14:20 +0530

    fix: force X-Robots-Tag index follow header

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,21 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   images: {
-//     remotePatterns: [{ protocol: "https", hostname: "cdn.sanity.io" }],
-//   },
-//   eslint: {
-//     ignoreDuringBuilds: true, // ← lets the build pass even if lint errors exist
-//   },
-// };
-
-// export default nextConfig;
-
-
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ protocol: "https", hostname: "cdn.sanity.io" }],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "index, follow",
+          },
+        ],
+      },
+    ];
   },
 };
 
